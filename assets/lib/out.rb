8b3cc0ce7f9221d01071e68a687f480e6e587900
Chris Olstrom <chris@olstrom.com>
2016-02-15 10:07:16 -0800

    Add support for context in resource usage

--- assets/lib/out.rb
+++ assets/lib/out.rb
@@ -20,7 +20,7 @@ end
 repo = Repository.new(name: input['source']['repo'])
 pr   = repo.pull_request(id: id)
 
-pr.status!(state: input['params']['status'], atc_url: input['source']['base_url'])
+pr.status!(state: input['params']['status'], atc_url: input['source']['base_url'], input['source']['context'])
 
 json!(version: pr.as_json,
       metadata: [
