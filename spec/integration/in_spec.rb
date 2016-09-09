a0ea010a836133a4f44dc9b75b5b8a0f0c43abf0
JT Archie <jtarchie@gmail.com>
2016-09-08 19:12:18 -0600

    add tests for failing case of fetch merge

--- spec/integration/in_spec.rb
+++ spec/integration/in_spec.rb
@@ -21,9 +21,6 @@ describe 'get' do
   end
 
   before do
-    proxy.stub('https://api.github.com:443/repos/jtarchie/test/pulls/1')
-         .and_return(json: { html_url: 'http://example.com', number: 1, head: { ref: 'foo' } })
-
     git('init -q')
     @ref = commit('init')
     commit('second')
@@ -31,65 +28,96 @@ describe 'get' do
     git("update-ref refs/pull/1/head #{@ref}")
   end
 
-  it 'checks out the pull request to dest_dir' do
-    get(version: { ref: @ref, pr: '1' }, source: { access_token: 'abc', uri: git_uri, repo: 'jtarchie/test' })
-    expect(@ref).to eq git('log --format=format:%H HEAD', dest_dir)
-  end
+  context 'for every PR that is checked out' do
+    before do
+      proxy.stub('https://api.github.com:443/repos/jtarchie/test/pulls/1')
+           .and_return(json: { html_url: 'http://example.com', number: 1, head: { ref: 'foo' } })
+    end
 
-  it 'returns the correct JSON metadata' do
-    output, = get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, repo: 'jtarchie/test' })
-    expect(output).to eq('version'  => { 'ref' => @ref, 'pr' => '1' },
-                         'metadata' => [{
-                           'name' => 'url',
-                           'value' => 'http://example.com'
-                         }])
-  end
+    it 'checks out the pull request to dest_dir' do
+      get(version: { ref: @ref, pr: '1' }, source: { access_token: 'abc', uri: git_uri, repo: 'jtarchie/test' })
+      expect(@ref).to eq git('log --format=format:%H HEAD', dest_dir)
+    end
 
-  it 'adds metadata to `git config`' do
-    get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, repo: 'jtarchie/test' })
+    it 'returns the correct JSON metadata' do
+      output, = get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, repo: 'jtarchie/test' })
+      expect(output).to eq('version'  => { 'ref' => @ref, 'pr' => '1' },
+                           'metadata' => [{
+                             'name' => 'url',
+                             'value' => 'http://example.com'
+                           }])
+    end
 
-    value = git('config --get pullrequest.url', dest_dir)
-    expect(value).to eq 'http://example.com'
-  end
+    it 'adds metadata to `git config`' do
+      get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, repo: 'jtarchie/test' })
 
-  it 'checks out as a branch' do
-    get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, repo: 'jtarchie/test' })
+      value = git('config --get pullrequest.url', dest_dir)
+      expect(value).to eq 'http://example.com'
+    end
 
-    value = git('rev-parse --abbrev-ref HEAD', dest_dir)
-    expect(value).to eq 'foo'
-  end
+    it 'checks out as a branch' do
+      get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, repo: 'jtarchie/test' })
 
-  it 'checks out as a branch when fetch_merge is false' do
-    get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, fetch_merge: false, repo: 'jtarchie/test' })
+      value = git('rev-parse --abbrev-ref HEAD', dest_dir)
+      expect(value).to eq 'foo'
+    end
 
-    value = git('rev-parse --abbrev-ref HEAD', dest_dir)
-    expect(value).to eq 'foo'
-  end
+    it 'sets config variable to branch name' do
+      get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, repo: 'jtarchie/test' })
+      value = git('config pullrequest.branch', dest_dir)
+      expect(value).to eq 'foo'
+    end
 
-  it 'checks out as a merged branch when fetch_merge is true' do
-    get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, fetch_merge: true, repo: 'jtarchie/test' })
+    context 'when the git clone fails' do
+      it 'provides a helpful erorr message' do
+        _, error = get(version: { ref: @ref, pr: '1' }, source: { uri: 'invalid_git_uri', repo: 'jtarchie/test' })
+        expect(error).to include 'git clone failed'
+      end
+    end
 
-    value = git('rev-parse --abbrev-ref HEAD', dest_dir)
-    expect(value).to eq 'master'
-  end
-
-  it 'sets config variable to branch name' do
-    get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, repo: 'jtarchie/test' })
-    value = git('config pullrequest.branch', dest_dir)
-    expect(value).to eq 'foo'
-  end
-
-  context 'when the git clone fails' do
-    it 'provides a helpful erorr message' do
-      _, error = get(version: { ref: @ref, pr: '1' }, source: { uri: 'invalid_git_uri', repo: 'jtarchie/test' })
-      expect(error).to include 'git clone failed'
+    context 'when `every` is not defined' do
+      it 'shows a deprecation warning' do
+        _, error = get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, repo: 'jtarchie/test' })
+        expect(error).to include 'DEPRECATION: Please note that you should update to using `version: every` on your `get` for this resource.'
+      end
     end
   end
 
-  context 'when `every` is not defined' do
-    it 'shows a deprecation warning' do
-      _, error = get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, repo: 'jtarchie/test' })
-      expect(error).to include 'DEPRECATION: Please note that you should update to using `version: every` on your `get` for this resource.'
+  context 'when the PR is meregable' do
+    context 'and fetch_merge is false' do
+      it 'checks out as a branch named in the PR' do
+        proxy.stub('https://api.github.com:443/repos/jtarchie/test/pulls/1')
+             .and_return(json: { html_url: 'http://example.com', number: 1, head: { ref: 'foo' }, mergeable: true })
+
+        get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, fetch_merge: false, repo: 'jtarchie/test' })
+
+        value = git('rev-parse --abbrev-ref HEAD', dest_dir)
+        expect(value).to eq 'foo'
+      end
+    end
+
+    context 'and fetch_merge is true' do
+      it 'checks out the branch the PR would be merged into' do
+        proxy.stub('https://api.github.com:443/repos/jtarchie/test/pulls/1')
+             .and_return(json: { html_url: 'http://example.com', number: 1, head: { ref: 'foo' }, mergeable: true })
+
+        get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, fetch_merge: true, repo: 'jtarchie/test' })
+
+        value = git('rev-parse --abbrev-ref HEAD', dest_dir)
+        expect(value).to eq 'master'
+      end
+    end
+  end
+
+  context 'when the PR is not mergeable' do
+    context 'and fetch_merge is true' do
+      it 'raises a helpful error message' do
+        proxy.stub('https://api.github.com:443/repos/jtarchie/test/pulls/1')
+             .and_return(json: { html_url: 'http://example.com', number: 1, head: { ref: 'foo' }, mergeable: false })
+
+        _, error = get(version: { ref: @ref, pr: '1' }, source: { uri: git_uri, fetch_merge: true, repo: 'jtarchie/test' })
+        expect(error).to include 'PR has merge conflicts'
+      end
     end
   end
 end
