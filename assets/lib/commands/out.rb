d6bcac7cb52262ae155488a748bcbc7a61111f21
Yonatan Kiron <yonatankiron@gmail.com>
2018-06-07 16:00:38 +0300

    Add delete label parameter

--- assets/lib/commands/out.rb
+++ assets/lib/commands/out.rb
@@ -71,6 +71,17 @@ module Commands
         metadata << { 'name' => 'label', 'value' => params.label }
       end
 
+      if params.delete_label
+        label_exists = Octokit.labels_for_issue(input.source.repo, id).select do |hash|
+          hash.to_hash[:name] == params.delete_label
+        end
+
+        if label_exists.any?
+          Octokit.remove_label(input.source.repo, id, params.delete_label)
+        end
+        metadata << { 'name' => 'delete_label', 'value' => params.delete_label }
+      end
+
       if params.merge.method
         commit_msg = if params.merge.commit_msg
                        commit_path = File.join(destination, params.merge.commit_msg)
